f3d35f5e4e35c732ed0725d7b0480ebfd9c1c6d9
ak-m <ak.ghub.m@gmail.com>
2024-02-26 20:02:39 -0500

    updated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-ui-rest-jph4';
+  title = 'ang-ui-rest-jph4-updated';
 }
